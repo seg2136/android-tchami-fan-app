9bc6e8ca045d7f902dcc4172e3cf1c7d9f68e138
Stephany Garcia <seg2136@gmail.com>
2015-10-22 14:17:17 -0700

    Includes About Activity and Shows Activity

--- app/src/main/java/com/example/guest/tchamitchami/MainActivity.java
+++ app/src/main/java/com/example/guest/tchamitchami/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.guest.tchamitchami;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mAboutButton;
+    private Button mShowsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAboutButton = (Button) findViewById(R.id.aboutButton);
+        mShowsButton = (Button) findViewById(R.id.showsButton);
+
+        mAboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mShowsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ShowsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
